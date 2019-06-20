2219b8895ce33c8f04f0088e6b714e1828b52ff7
Peter Solymos <psolymos@gmail.com>
2019-06-19 19:44:02 -0600

    plot updates

--- R/plot.bsims_detections.R
+++ R/plot.bsims_detections.R
@@ -1,5 +1,6 @@
 plot.bsims_detections <-
-function(x, first_only=TRUE, tlim=NULL,
+function(x,
+event_type=c("vocal", "move", "both"), tlim=NULL,
 pch_nest=3, col_nest="darkgreen", cex_nest=1,
 pch_vocal=21, col_vocal="blue", cex_vocal=0.5,
 lty_move=1, col_move="orange", lwd_move=1,
@@ -14,6 +15,6 @@ lty_det=1, col_det="black", lwd_det=1,
     pch_vocal=pch_vocal, col_vocal=col_vocal, cex_vocal=cex_vocal,
     lty_move=lty_move, col_move=col_move, lwd_move=lwd_move, ...)
   if (!is.na(lty_det))
-    lines(x, first_only, tlim=tlim, col=col_det, lty=lty_det, ...)
+    lines(x, event_type=event_type, tlim=tlim, col=col_det, lty=lty_det, ...)
   invisible(x)
 }
