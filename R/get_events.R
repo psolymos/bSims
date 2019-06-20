fa3f6c8722ddec43c1d8f3353802ef2fac61acc1
Peter Solymos <psolymos@gmail.com>
2019-06-19 18:28:27 -0600

    events extraction tweaked

--- R/get_events.R
+++ R/get_events.R
@@ -1,32 +1,22 @@
 get_events <-
 function(x, event_type=c("vocal", "move", "both"), tlim=NULL) {
+  z <- .get_raw_events(x)
+  if (nrow(z) == 0)
+    return(z)
   event_type <- match.arg(event_type)
-  if (sum(x$abundance) == 0)
-    return(data.frame(
-      x=numeric(0),
-      y=numeric(0),
-      t=numeric(0),
-      v=numeric(0),
-      i=numeric(0)
-    ))
   if (is.null(tlim))
     tlim <- c(0, x$duration)
+  if (length(tlim) != 2)
+    stop("tlim must be a vector of length 2")
   tlim <- pmin(pmax(0, tlim), x$duration)
-  z <- lapply(1:length(x$events), function(i) {
-    zz <- x$events[[i]]
-    zz$i <- rep(i, nrow(zz))
-    zz
-  })
-  z <- do.call(rbind, z)
-  z <- z[order(z$t),]
   keep <- switch(event_type,
     "vocal"=z$v > 0,
     "move"=z$v == 0,
     "both"=rep(TRUE, nrow(z)))
-  rownames(z) <- NULL
   z$x <- x$nests$x[z$i] + z$x
   z$y <- x$nests$y[z$i] + z$y
   z <- z[z$t %[]% tlim,,drop=FALSE]
   attr(z, "event_type") <- event_type
+  attr(z, "tlim") <- tlim
   z
 }
---
+++ R/get_raw_events.R
@@ -0,0 +1,20 @@
+.get_raw_events <- function(x) {
+  if (sum(x$abundance) == 0 || is.null(x$events)) {
+    z <- data.frame(
+      x=numeric(0),
+      y=numeric(0),
+      t=numeric(0),
+      v=numeric(0),
+      i=numeric(0))
+  } else {
+    z <- lapply(seq_len(length(x$events)), function(i) {
+      zz <- x$events[[i]]
+      zz$i <- rep(i, nrow(zz))
+      zz
+    })
+    z <- do.call(rbind, z)
+    z <- z[order(z$t),]
+  }
+  rownames(z) <- NULL
+  z
+}
