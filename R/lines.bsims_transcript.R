07d800cd9f2585cc81bbcf5c8667aea998ad95e7
Peter Solymos <psolymos@gmail.com>
2019-06-22 00:40:37 -0600

    added lines

---
+++ R/lines.bsims_transcript.R
@@ -0,0 +1,10 @@
+lines.bsims_detections <-
+function(x, event_type=c("vocal", "move", "both"), tlim=NULL, ...) {
+  z <- get_detections(x,
+    condition=x$conditio, event_type=event_type, tlim=tlim)
+  xy <- z[,c("x", "y")]
+  for (i in seq_len(nrow(z))) {
+    lines(c(x$xy[1L], xy[i,1L]), c(x$xy[2L], xy[i,2L]), ...)
+  }
+  invisible(x)
+}
