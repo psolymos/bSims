f0321bed4ab9dddcb622193500e1e07788217def
Peter Solymos <psolymos@gmail.com>
2019-09-13 14:47:43 -0600

    rename

--- inst/shiny/distfunH.R
+++ inst/shiny/distfunH.R
@@ -1,13 +1,18 @@
 library(shiny)
 
 ui <- navbarPage("bSims",
-  tabPanel("Detection function",
+  tabPanel("Detection function (H)",
     plotOutput(outputId = "plot_dfun"),
-    checkboxInput("hazard", "Hazard rate formulation (1-exp(...))"),
-    sliderInput("tau", "tau", 0, 5, 1, 0.1),
-    sliderInput("b", "b", 0, 10, 2, 0.25),
-    sliderInput("rmax", "r max", 0, 10, 2, 0.25),
-    verbatimTextOutput("settings")
+    hr(),
+    column(6,
+      checkboxInput("hazard", "Hazard rate formulation (1-exp(...))"),
+      sliderInput("tau", "tau", 0, 5, 1, 0.1),
+      sliderInput("b", "b", 0, 10, 2, 0.25),
+      sliderInput("rmax", "r max", 0, 10, 2, 0.25)
+    ),
+    column(6,
+      verbatimTextOutput("settings")
+    )
   )
 )
 
@@ -19,8 +24,13 @@ server <- function(input, output) {
     } else {
       function(d) exp(-(d/input$tau)^input$b)
     }
-    plot(d, g(d), type="l", col=4, ylim=c(0,1),
+    plot(d, g(d), type="n", ylim=c(0,1), axes=FALSE,
       xlab="Distance (100 m)", ylab="P(detection)")
+    axis(1)
+    axis(2)
+    polygon(c(0, max(d), max(d), 0), c(0, 0, 1, 1),
+      border=NA, col="darkolivegreen1")
+    lines(d, g(d), col=2, lwd=3)
   })
   output$settings <- renderText({
     c(
