c9afffdcd5f0be33d723a88e56ca5a68c8ddfe1f
Peter Solymos <psolymos@gmail.com>
2019-11-21 17:00:14 -0700

    address special cases

--- R/print.bsims_all.R
+++ R/print.bsims_all.R
@@ -5,8 +5,19 @@ function(x, ...) {
     m <- max(n)
     cat("bSims wrapper object with settings:\n")
     for (i in seq_along(s)) {
-        cat("  ", names(s)[i], rep(" ", m-n[i]), ": ", 
-			paste(s[[i]], collapse=", "), "\n", sep="")
+        if (is.null(s[[i]])) {
+            val <- "NULL"
+        } else {
+            if (is.function(s[[i]])) {
+                val <- paste(deparse(s[[i]]), collapse="")
+                if (nchar(val) > 50)
+                    val <- paste0(substr(val, 1, 50), " [...]")
+            } else {
+                val <- paste(s[[i]], collapse=", ")
+            }
+        }
+        cat("  ", names(s)[i], rep(" ", m-n[i]), ": ",
+            val, "\n", sep="")
     }
     invisible(x)
 }
