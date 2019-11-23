06c06839bedba8cf2855499bd4844e7b6de4e908
Peter Solymos <psolymos@gmail.com>
2019-11-23 00:03:27 -0700

    expand_list added

---
+++ R/expand_list.R
@@ -0,0 +1,23 @@
+expand_list <-
+function(...) {
+  a <- list(...)
+  if (is.null(names(a)) || any(names(a) == ""))
+    stop("all arguments must be named")
+  ## expand.grid call with indices
+  v <- call("expand.grid")
+  for (i in seq_along(a)) {
+    v[[names(a)[i]]] <- seq_along(a[[i]])
+  }
+  d <- eval(v)
+  ## output list
+  out <- list()
+  for (j in seq_len(nrow(d))) {
+    l <- list()
+    for (k in seq_len(ncol(d))) {
+      h <- colnames(d)[k]
+      l[[h]] <- a[[h]][[d[j,h]]]
+    }
+    out[[j]] <- l
+  }
+  out
+}
