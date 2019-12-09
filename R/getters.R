6aa631e81e9497dc7efb65aa826587318d5d3db0
Peter Solymos <psolymos@gmail.com>
2019-12-08 19:00:18 -0700

    getters rely on input object

--- R/getters.R
+++ R/getters.R
@@ -1,20 +1,12 @@
 get_nests <- function (x, ...)
   UseMethod("get_nests")
-get_nests.bsims_population <- function (x, ...)
-  x$nests[,c("x", "y")]
-
-get_events <- function (x, ...)
-  UseMethod("get_events")
-get_events.bsims_events <- function (x, ...) {
-  out <- .get_events(x, ...)
-  out$d <- NULL
+get_nests.bsims_population <- function (x, ...) {
+  out <- x$nests
+  ## behav groups not defined for pop object
+  if (is.null(out$g))
+    out$g <- NA
   out
 }
-
-get_detections <- function (x, ...)
-  UseMethod("get_detections")
-get_detections.bsims_detections <- function (x, ...)
-  .get_detections(x, ...)
 
 get_abundance <- function (x, ...)
   UseMethod("get_abundance")
@@ -25,3 +17,27 @@ get_density <- function (x, ...)
   UseMethod("get_density")
 get_density.bsims_population <- function (x, ...)
   sum(x$abundance) / x$extent^2
+
+get_events <- function (x, ...)
+  UseMethod("get_events")
+get_events.bsims_events <- function (x, ...) {
+  ## event type is not defined for events object
+  event_type <- if (is.null(x$event_type))
+    "both" else x$event_type
+  out <- .get_events(x, event_type=event_type, ...)
+  ## remove true distances
+  out$d <- NULL
+  out
+}
+
+get_detections <- function (x, ...)
+  UseMethod("get_detections")
+get_detections.bsims_detections <- function (x, ...) {
+  ## condition is not defined for detection object
+  condition <- if (is.null(x$condition))
+    "alldet" else x$condition
+  .get_detections(x,
+    event_type=x$event_type,
+    condition=condition,
+    perception=x$perception, ...)
+}
