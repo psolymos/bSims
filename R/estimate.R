e34843468b6d7f07ac252fd5e283fdd73c22cb1a
Peter Solymos <psolymos@gmail.com>
2019-12-09 17:16:23 -0700

    adding estimate method

---
+++ R/estimate.R
@@ -0,0 +1,30 @@
+estimate <- function (object, ...)
+  UseMethod("estimate")
+estimate.bsims_transcript <- function (object, ...) {
+  y <- get_table(object, "removal")
+  rmax <- max(object$rint)
+  tmax <- max(object$tint)
+
+  fitp <- detect::cmulti.fit(
+    matrix(colSums(y), 1),
+    matrix(object$tint, 1),
+    type="rem")
+  phihat <- exp(fitp$coef)
+
+  fitq <- detect::cmulti.fit(
+    matrix(rowSums(y), 1),
+    matrix(object$rint, 1),
+    type="dis")
+  tauhat <- exp(fitq$coef)
+
+  p <- 1-exp(-tmax*phihat)
+  if (is.infinite(rmax)) {
+    A <- pi * tauhat^2
+    q <- 1
+  } else {
+    q <- (tauhat^2/rmax^2) * (1-exp(-(rmax/tauhat)^2))
+    A <- pi * rmax^2
+  }
+  Dhat <- sum(y) / (A * p * q)
+  c(phi=phihat, tau=tauhat, density=Dhat)
+}
