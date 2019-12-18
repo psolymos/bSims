224014c077c86007b8b31cd778e8035c2e794a0a
Peter Solymos <psolymos@gmail.com>
2019-12-18 14:50:30 -0700

    examples added back

--- tests/tests.R
+++ tests/tests.R
@@ -1,5 +1,23 @@
 #remotes::install_github("psolymos/bSims")
 
+library(bSims)
+
+help_pages <- c(
+  "acceptreject",
+  "bsims_init",
+  "dist_fun2",
+  "events",
+  "expand_list",
+  "get_nests",
+  "rlnorm2",
+  "rmvn")
+
+for (i in help_pages) {
+  cat("\n\n---------- bSims example:", i, "----------\n\n")
+  eval(parse(text=paste0("example('", i,
+    "', package='bSims', run.dontrun=TRUE, run.donttest=TRUE)")))
+}
+
 cat("\n\n---------- Parsing Shiny apps:", "----------\n\n")
 files <- list.files(system.file("shiny", package="bSims"),
                     pattern="\\.R$")
