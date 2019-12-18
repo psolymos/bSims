6d5f87013e0a89939f52e3259c0e222ea8d406c6
Peter Solymos <psolymos@gmail.com>
2019-12-18 15:49:58 -0700

    Nmix example: all good !

--- inst/tmp/visits.R
+++ inst/tmp/visits.R
@@ -3,59 +3,76 @@
 library(bSims)
 library(dclone)
 library(rjags)
-model <- custommodel("model {
-    for (i in 1:n) {
-        N[i] ~ dpois(D*A)
-        for (t in 1:T) {
-            Y[i,t] ~ dbin(p, N[i])
-        }
-    }
-    p ~ dunif(0.001, 0.999)
-    D ~ dlnorm(0, 0.001)
-}")
+
+## settings
+tint <- c(2,4,6,8,10)
+rint <- c(0.5, 1, 1.5)
 phi <- 0.5
 tau <- 1
 dur <- 10
 l <- bsims_init()
 D <- 1
 p <- 1-exp(-2*phi)
-q <- (tau^2/1.5^2) * (1-exp(-(1.5/tau)^2))
+q <- (tau^2/max(rint)^2) * (1-exp(-(max(rint)/tau)^2))
 P <- p * q
 
+## simulated data
 Yq1 <- NULL
 Yq2 <- NULL
+Yq3 <- NULL
+f <- function(d, tau, ...) q
 for (i in 1:1000) {
   n <- bsims_populate(l, D)
   a <- bsims_animate(n, vocal_rate=phi, duration=dur)
   o <- bsims_detect(a, tau=tau)
-  ## distance
-  x1 <- bsims_transcribe(o, c(2,4,6,8,10), c(0.5, 1, 1.5))
+  ## distance: detectability is function of distance
+  x1 <- bsims_transcribe(o, tint, rint)
   Yq1 <- rbind(Yq1, colSums(x1$visits))
-  ## binomial
-  x2 <- bsims_transcribe(a, c(2,4,6,8,10), c(0.5, 1, 1.5))
+  ## binomial: detectability does not depend on distance
+  x2 <- bsims_transcribe(a, tint, rint)
   Yq2 <- rbind(Yq2, rbinom(5, colSums(x2$visits), q))
+  ## constant and <1 distance function
+  o3 <- bsims_detect(a, tau=tau, dist_fun = f)
+  x3 <- bsims_transcribe(o3, tint, rint)
+  Yq3 <- rbind(Yq3, colSums(x3$visits))
 }
-c(mean(Yq1), mean(Yq2), sd(Yq1), sd(Yq2))
-D*1.5^2*pi*p*q
+c(mean(Yq1), mean(Yq2), mean(Yq3), sd(Yq1), sd(Yq2), sd(Yq3))
+D*max(rint)^2*pi*p*q
+
+## density estimation
+model <- custommodel("model {
+    for (i in 1:n) {
+        N[i] ~ dpois(D*A)
+        for (j in 1:J) {
+            Y[i,j] ~ dbin(p, N[i])
+        }
+    }
+    p ~ dunif(0.001, 0.999)
+    D ~ dlnorm(0, 0.001)
+}")
 
 ## distance
-Y <- Yq1
-dat <- list(Y = Y, A=1.5^2*pi, n = nrow(Y), T = ncol(Y))
-ini <- list(N = apply(Y, 1, max) + 1)
-fit <- jags.fit(data = dat, params = c("p", "D"),
+dat1 <- list(Y = Yq1, A=max(rint)^2*pi, n = nrow(Yq1), J = ncol(Yq1))
+ini1 <- list(N = apply(Yq1, 1, max) + 1)
+fit1 <- jags.fit(data = dat1, params = c("p", "D"),
     n.update = 1000,
-    model = model, inits = ini)
-coef(fit)
-D
-mean(Y)/(1.5^2*pi*p*q)
+    model = model, inits = ini1)
+
 ## binomial
-Y <- Yq2
-dat <- list(Y = Y, A=1.5^2*pi, n = nrow(Y), T = ncol(Y))
-ini <- list(N = apply(Y, 1, max) + 1)
-fit <- jags.fit(data = dat, params = c("p", "D"),
+dat2 <- list(Y = Yq2, A=max(rint)^2*pi, n = nrow(Yq2), J = ncol(Yq2))
+ini2 <- list(N = apply(Yq2, 1, max) + 1)
+fit2 <- jags.fit(data = dat2, params = c("p", "D"),
     n.update = 1000,
-    model = model, inits = ini)
-coef(fit)
-D
-mean(Y)/(1.5^2*pi*p*q)
+    model = model, inits = ini2)
+
+## constant dfun
+dat3 <- list(Y = Yq3, A=max(rint)^2*pi, n = nrow(Yq3), J = ncol(Yq3))
+ini3 <- list(N = apply(Yq3, 1, max) + 1)
+fit3 <- jags.fit(data = dat3, params = c("p", "D"),
+    n.update = 1000,
+    model = model, inits = ini3)
+
+## summaries
+c(mean(Yq2)/(max(rint)^2*pi*p*q), mean(Yq1)/(max(rint)^2*pi*p*q), mean(Yq3)/(max(rint)^2*pi*p*q))
+cbind(True=c(D=D, pq=p*q), Nmix1=coef(fit2), Nmix2=coef(fit2), Nmix3=coef(fit3))
 
