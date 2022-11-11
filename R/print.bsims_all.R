7dfdb856426a8376e6f4f23551543653ec5d67ac
Peter Solymos <psolymos@gmail.com>
2022-11-11 08:50:13 -0700

    Print default object better, closes #18

--- R/print.bsims_all.R
+++ R/print.bsims_all.R
@@ -2,22 +2,26 @@ print.bsims_all <-
 function(x, ...) {
     s <- x$settings()
     n <- nchar(names(s))
-    m <- max(n)
-    cat("bSims wrapper object with settings:\n")
-    for (i in seq_along(s)) {
-        if (is.null(s[[i]])) {
-            val <- "NULL"
-        } else {
-            if (is.function(s[[i]])) {
-                val <- paste(deparse(s[[i]]), collapse="")
-                if (nchar(val) > 50)
-                    val <- paste0(substr(val, 1, 50), " [...]")
-            } else {
-                val <- paste(s[[i]], collapse=", ")
-            }
-        }
-        cat("  ", names(s)[i], rep(" ", m-n[i]), ": ",
-            val, "\n", sep="")
+    if (length(s) > 0L) {
+      m <- max(n)
+      cat("bSims wrapper object with settings:\n")
+      for (i in seq_along(s)) {
+          if (is.null(s[[i]])) {
+              val <- "NULL"
+          } else {
+              if (is.function(s[[i]])) {
+                  val <- paste(deparse(s[[i]]), collapse="")
+                  if (nchar(val) > 50)
+                      val <- paste0(substr(val, 1, 50), " [...]")
+              } else {
+                  val <- paste(s[[i]], collapse=", ")
+              }
+          }
+          cat("  ", names(s)[i], rep(" ", m-n[i]), ": ",
+              val, "\n", sep="")
+      }
+    } else {
+      cat("bSims wrapper object with default settings\n")
     }
     invisible(x)
 }
