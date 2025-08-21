66f992e4eb14828a3f981afc2e8e391dba9618c1
Peter Solymos <psolymos@gmail.com>
2025-08-20 18:25:39 -0600

    Add tgz sources
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- inst/tmp/shinylive.R
+++ inst/tmp/shinylive.R
@@ -1,8 +1,10 @@
 # clean up output folder
-unlink("apps/shinylive", recursive = TRUE)
+unlink("apps", recursive = TRUE)
 
 # make shinylive apps
 for (app in c("bsimsH", "bsimsHER", "distfunH", "distfunHER")) {
+    # unlink(sprintf("apps/shiny/%s", app))
+    # unlink(sprintf("apps/%s", app))
     dir.create(sprintf("apps/shiny/%s/www", app), recursive = TRUE, showWarnings = FALSE)
     file.copy(sprintf("inst/shiny/%s.R", app), sprintf("apps/shiny/%s/app.R", app), overwrite = TRUE)
     file.copy("inst/shiny/www/clipboard.min.js", sprintf("apps/shiny/%s/www/clipboard.min.js", app), overwrite = TRUE)
